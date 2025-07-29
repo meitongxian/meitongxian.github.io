4e44e5812ec5e78ca506cc5386a33f984798b8bc
Maggie Xian <111546244+meitongxian@users.noreply.github.com>
2025-07-29 01:59:57 -0500

    July 2025 update

--- js/type-demo.js
+++ js/type-demo.js
@@ -5,16 +5,23 @@ let sliders = document.querySelectorAll(".slider");
 let sliderValues = document.querySelectorAll(".output");
 
 for (let i = 0; i < sliders.length; i++) {
-    sliderValues[i].innerHTML = sliders[i].value;
+  sliderValues[i].innerHTML = sliders[i].value;
 }
 
 sliders.forEach(slider => {
     let sliderIndex = slider.getAttribute("data-index");
     let output = document.querySelector(`.output[data-index="${sliderIndex}"]`);
     slider.oninput = function() {
-      demoWrapper.forEach(demo => { 
-        demo.style.setProperty(`--${this.id}`, this.value);
-        output.innerHTML = this.value;
-      });
+      if (sliderIndex === "4" || sliderIndex === "5") {
+        demoWrapper.forEach(demo => { 
+          demo.style.setProperty(`--${this.id}`, this.value + "px");
+          output.innerHTML = this.value;
+        });
+      } else {
+        demoWrapper.forEach(demo => { 
+          demo.style.setProperty(`--${this.id}`, this.value);
+          output.innerHTML = this.value;
+        });
+      }
     };
 });
